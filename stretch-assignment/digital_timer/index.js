33939e38aa3b77dc08d00dfff5d154eb1fbb9538
Alexander Sierra <alexandercsierra@gmail.com>
2019-12-02 01:21:17 -0500

    counter counts seconds appropriately without upper limit

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -3,18 +3,44 @@
 
 let msTens = document.querySelector("#msTens");
 let msHundreds = document.querySelector("#msHundreds");
+let secondOnes = document.querySelector("#secondOnes");
+let secondTens = document.querySelector("#secondTens");
+
+
 
 setInterval(function(){ 
     if (msTens.textContent === "-"){
         msTens.textContent = 0;
         msHundreds.textContent = 0;
+        secondOnes.textContent = 0;
+        secondTens.textContent = 0;
     } else if (msTens.textContent < 9){
         msTens.textContent++;
     } else if (msTens.textContent = 9){
         msTens.textContent = 0;
-        msHundreds.textContent++;
+        if (msHundreds.textContent < 9){
+            msHundreds.textContent++;
+        } else {
+            msHundreds.textContent = 0;
+            if (secondOnes.textContent < 9){
+                secondOnes.textContent++;
+            } else {
+                secondOnes.textContent = 0;
+                if (secondTens.textContent < 9){
+                    secondTens.textContent++;
+                } else {
+                    secondTens.textContent = 0;
+                    
+                }
+                
+            }
+        }
     }
-
+        
+    // } else if (msHundreds.textContent = 9){
+    //     msHundreds.textContent = 0;
+    //     console.log("seconds should change");
+    // }
 
 
 }, 10);
