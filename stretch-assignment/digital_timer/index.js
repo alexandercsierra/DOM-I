277299709ccb8a19ed66ccd467f24b0d20d84208
alexandercsierra <alexandercsierra@gmail.com>
2019-12-02 16:08:56 -0500

    fixed bug that made each interval faster

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -5,60 +5,74 @@ let msTens = document.querySelector("#msTens");
 let msHundreds = document.querySelector("#msHundreds");
 let secondOnes = document.querySelector("#secondOnes");
 let secondTens = document.querySelector("#secondTens");
+let isReset = false;
 
 
 function timer(){
-var counter = setInterval(function(){ 
-    if (msTens.textContent === "-"){
-        msTens.textContent = 0;
-        msHundreds.textContent = 0;
-        secondOnes.textContent = 0;
-        secondTens.textContent = 0;
-    } else if (msTens.textContent < 9){
-        msTens.textContent++;
-    } else if (msTens.textContent = 9){
-        msTens.textContent = 0;
-        if (msHundreds.textContent < 9){
-            msHundreds.textContent++;
-        } else {
-            msHundreds.textContent = 0;
-            if (secondOnes.textContent < 9){
-                secondOnes.textContent++;
-            } else {
+    clearInterval(counter);
+    var counter = setInterval(function(){ 
+        if (isReset === false){
+            if (msTens.textContent === "-"){
+                msTens.textContent = 0;
+                msHundreds.textContent = 0;
                 secondOnes.textContent = 0;
-                if (secondTens.textContent = 1){
-                    // secondTens.textContent++;
-                    msTens.style.color = "red";
-                    msHundreds.style.color = "red";
-                    secondOnes.style.color = "red";
-                    secondTens.style.color = "red";
-                    clearInterval(counter);
-                } 
-                
+                secondTens.textContent = 0;
+            } else if (msTens.textContent < 9){
+                msTens.textContent++;
+            } else if (msTens.textContent = 9){
+                msTens.textContent = 0;
+                if (msHundreds.textContent < 9){
+                    msHundreds.textContent++;
+                } else {
+                    msHundreds.textContent = 0;
+                    if (secondOnes.textContent < 9){
+                        secondOnes.textContent++;
+                    } else {
+                        secondOnes.textContent = 0;
+                        if (secondTens.textContent = 1){
+                            msTens.style.color = "red";
+                            msHundreds.style.color = "red";
+                            secondOnes.style.color = "red";
+                            secondTens.style.color = "red";
+                            clearInterval(counter);
+                        } 
+                        
+                    }
+                }
             }
+            
+
+
+        } else {
+            msTens.textContent = "0";
+            msHundreds.textContent = "0";
+            secondOnes.textContent = "0";
+            secondTens.textContent = "0";
+            msTens.style.color = "black";
+            msHundreds.style.color = "black";
+            secondOnes.style.color = "black";
+            secondTens.style.color = "black";
+            clearInterval(counter);
         }
-    }
-        
-    // } else if (msHundreds.textContent = 9){
-    //     msHundreds.textContent = 0;
-    //     console.log("seconds should change");
-    // }
-
-
-}, 10);
+    }, 10);
 }
 
 let start = document.querySelector(".start");
 start.addEventListener("click", e => {
+    isReset = false;
+    clearInterval(timer);
     timer();
     start.disabled = true;
+    start.style.background = "#517355";
 });
 
 let reset = document.querySelector(".reset");
 reset.addEventListener("click", e => {
-    if (secondTens.style.color === "red"){
-        start.disabled = false;
-    }
+    isReset = true;
+    clearInterval(timer);
+    start.disabled = false;
+    start.style.background = "#26c73e";
+        
     
     msTens.textContent = "0";
     msHundreds.textContent = "0";
@@ -68,5 +82,7 @@ reset.addEventListener("click", e => {
     msHundreds.style.color = "black";
     secondOnes.style.color = "black";
     secondTens.style.color = "black";
+    
+    
 
 });
